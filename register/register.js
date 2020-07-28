b798a9054b4c61d2618b38330f32ed0bb6dbbadd
Esha <esha.gupta@somaiya.edu>
2020-07-28 15:24:21 +0530

    Removed: unwanted lines

--- register/register.js
+++ register/register.js
@@ -2,14 +2,14 @@
     var email=document.forms["RegisterForm"]["emailId"].value;
     var email_patt=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    var password =  document.forms["RegisterForm"]["registerPassword"].value;
-    alert(password);
-    var passwordpattern=/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-    
     var clg_name = document.forms["RegisterForm"]["collegeName"].value;
     var userPattern = /^[a-zA-Z]+$/;
 
+    var password =  document.forms["RegisterForm"]["registerPassword"].value;
+    var passwordpattern=/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
     var confirm_pass=document.forms["RegisterForm"]["confirmPassword"].value;
+    
+    var studyYear=document.getElementById('yearOfStudy').value;
     
     if(email_patt.test(email) == false )
 	{
@@ -49,7 +49,16 @@
         document.getElementById('collegeNameAlert').innerHTML = '';
         document.getElementById('collegeNameAlert').style.display = 'none';
     }
-
+    if(studyYear == 'none')
+    {
+        document.getElementById('studyYearAlert').innerHTML=" **Please select a option";
+        return false;   
+    }
+    else{
+        document.getElementById('studyYearAlert').innerHTML = '';
+        document.getElementById('studyYearAlert').style.display = 'none';
+    }
+    //Validsation for Security Questions
     var q1 = document.forms["RegisterForm"]["question1"].value;
     var q2 = document.forms["RegisterForm"]["question2"].value;
     var q3 = document.forms["RegisterForm"]["question3"].value;
