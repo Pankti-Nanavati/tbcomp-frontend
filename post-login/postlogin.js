db367217140538bdc62eb0c8146fdc3a3ab2dbd4
kunjgala <kunjgala15@gmail.com>
2020-08-04 11:48:59 +0530

    updated buttons and carousel

--- post-login/postlogin.js
+++ post-login/postlogin.js
@@ -37,11 +37,11 @@ element.addEventListener
       btn2.style.color="black";
       btn3.style.color="black";
       btn1.style.width="15em";
-      btn2.style.width="10em";
-      btn3.style.width="10em";
+      btn2.style.width="5em";
+      btn3.style.width="5em";
       btn1.innerHTML = "BEC";
-      btn2.innerHTML = "E";
-      btn3.innerHTML = "E";
+      btn2.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
+      btn3.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
      
 }
 function myFunction2() {
@@ -52,19 +52,14 @@ function myFunction2() {
       btn1.style.color="black";
       btn3.style.color="black";
       btn2.style.backgroundColor="#2d9cc2";
-      btn2.style.color="white";
-      btn1.style.color="black";
-      btn3.style.color="black";
       btn2.style.width="15em";
-      btn1.style.width="10em";
-      btn3.style.width="10em";
+      btn1.style.width="5em";
+      btn3.style.width="5em";
       btn1.style.backgroundColor="white";
       btn3.style.backgroundColor="white";
       btn2.innerHTML = "EEEE";
-      btn1.innerHTML = "B";
-      btn3.innerHTML = "E";
-      btn1.classList.remove('btn1');
-	    btn2.classList.add('btn1j');
+      btn1.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
+      btn3.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
 }
 function myFunction3() {
       var btn1 = document.getElementById("btn1");
@@ -77,9 +72,9 @@ function myFunction3() {
       btn1.style.backgroundColor="white";
       btn2.style.backgroundColor="white";
       btn3.style.width="15em";
-      btn2.style.width="10em";
-      btn1.style.width="10em";
+      btn2.style.width="5em";
+      btn1.style.width="5em";
       btn3.innerHTML = "ECAD";
-      btn1.innerHTML = "B";
-      btn2.innerHTML = "E";
+      btn1.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
+      btn2.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
 }
