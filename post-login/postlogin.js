0cd30b8db50969d368100ef035e05613cd6f48d5
kunjgala <kunjgala15@gmail.com>
2020-08-07 12:39:11 +0530

    corrected buttons

--- post-login/postlogin.js
+++ post-login/postlogin.js
@@ -1,38 +1,22 @@
-var element = document . getElementById ( "btn1" ) ;
-element.addEventListener
-(
-    "click",
-    function ()
-    {
-        this . style . width = "15em" ;
-    }
-) ;
-var element = document . getElementById ( "btn2" ) ;
-element.addEventListener
-(
-    "click",
-    function ()
-    {
-        this . style . width = "15em" ;
-        this . style . color = "white";
-    }
-) ;
-var element = document . getElementById ( "btn3" ) ;
-element.addEventListener
-(
-    "click",
-    function ()
-    {
-        this . style . width = "15em" ;
-    }
-) ;
+
   function myFunction1() {
       var btn1 = document.getElementById("btn1");
       var btn2 = document.getElementById("btn2");
       var btn3 = document.getElementById("btn3");
+      btn2.style.backgroundImage="url('icon1.png')";
+      btn2.style.backgroundSize="1em 1.6em";
+      btn2.style.backgroundRepeat="no-repeat";
+      btn2.style.backgroundPosition="center";
+      btn2.innerText="";
+      btn3.style.backgroundImage="url('icon1.png')";
+      btn3.style.backgroundSize="1em 1.6em";
+      btn3.style.backgroundRepeat="no-repeat";
+      btn3.style.backgroundPosition="center";
+      btn3.innerText="";
       btn1.style.backgroundColor="#2d9cc2";
       btn2.style.backgroundColor="white";
       btn3.style.backgroundColor="white";
+      btn1.style.backgroundImage="none";
       btn1.style.color="white";
       btn2.style.color="black";
       btn3.style.color="black";
@@ -40,14 +24,23 @@ element.addEventListener
       btn2.style.width="4em";
       btn3.style.width="4em";
       btn1.innerHTML = "EEEE";
-      btn2.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
-      btn3.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
+     
      
 }
 function myFunction2() {
       var btn1 = document.getElementById("btn1");
       var btn2 = document.getElementById("btn2");
       var btn3 = document.getElementById("btn3");
+      btn1.style.backgroundImage="url('icon1.png')";
+      btn1.style.backgroundSize="1em 1.6em";
+      btn1.style.backgroundRepeat="no-repeat";
+      btn1.style.backgroundPosition="center";
+      btn1.innerText="";
+      btn3.style.backgroundImage="url('icon1.png')";
+      btn3.style.backgroundSize="1em 1.6em";
+      btn3.style.backgroundRepeat="no-repeat";
+      btn3.style.backgroundPosition="center";
+      btn3.innerText="";
       btn2.style.color="white";
       btn1.style.color="black";
       btn3.style.color="black";
@@ -57,24 +50,34 @@ function myFunction2() {
       btn3.style.width="4em";
       btn1.style.backgroundColor="white";
       btn3.style.backgroundColor="white";
+      btn2.style.backgroundImage="none";
       btn2.innerHTML = "BEC";
-      btn1.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
-      btn3.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
+     
 }
 function myFunction3() {
       var btn1 = document.getElementById("btn1");
       var btn2 = document.getElementById("btn2");
       var btn3 = document.getElementById("btn3");
+      btn1.style.backgroundImage="url('icon1.png')";
+      btn1.style.backgroundSize="1em 1.6em";
+      btn1.style.backgroundRepeat="no-repeat";
+      btn1.style.backgroundPosition="center";
+      btn1.innerText="";
+      btn2.style.backgroundImage="url('icon1.png')";
+      btn2.style.backgroundSize="1em 1.6em";
+      btn2.style.backgroundRepeat="no-repeat";
+      btn2.style.backgroundPosition="center";
+      btn2.innerText="";
       btn3.style.color="white";
       btn2.style.color="black";
       btn1.style.color="black";
       btn3.style.backgroundColor="#2d9cc2";
       btn1.style.backgroundColor="white";
       btn2.style.backgroundColor="white";
+      btn3.style.backgroundImage="none";
       btn3.style.width="8em";
       btn2.style.width="4em";
       btn1.style.width="4em";
       btn3.innerHTML = "ECAD";
-      btn1.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
-      btn2.innerHTML = "<img src=\'icon1.png\' width=\'16em\' height=\'25em\'>";
+     
 }
